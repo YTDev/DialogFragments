cda1696e04473506b7244878f5a09e2376c2d764
YTDev <belnisay@gmail.com>
2020-06-19 15:15:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dialogfragments/DialogFragment.java
@@ -0,0 +1,142 @@
+package com.example.dialogfragments;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DialogFragment extends androidx.fragment.app.DialogFragment {
+
+    private static final String ARG_TITLE="title";
+    private static final String ARG_MESSAGE="message";
+    private static final String ARG_ICON="icon";
+
+    private String title;
+    private String message;
+    private int icon;
+
+    private OnPositiveClickListener positiveClickListener;
+    private OnNegativeClickListener negativeClickListener;
+    private OnNeutralClickListener neutralClickListener;
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof OnPositiveClickListener){
+            positiveClickListener=(OnPositiveClickListener) context;
+        }
+        else
+            throw new RuntimeException("Please implement listener positive");
+
+        if(context instanceof OnNegativeClickListener){
+            negativeClickListener=(OnNegativeClickListener) context;
+        }
+        else
+            throw new RuntimeException("Please implement listener negative");
+
+        if(context instanceof OnNeutralClickListener){
+            neutralClickListener=(OnNeutralClickListener) context;
+        }
+        else
+            throw new RuntimeException("Please implement listener neutral");
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        positiveClickListener=null;
+        negativeClickListener=null;
+        neutralClickListener=null;
+    }
+
+    public static DialogFragment newInstance(String title, String message, int icon){
+        Bundle bundle=new Bundle();
+        bundle.putString(ARG_TITLE,title);
+        bundle.putString(ARG_MESSAGE,message);
+        bundle.putInt(ARG_ICON,icon);
+        DialogFragment dialogFragment=new DialogFragment();
+        dialogFragment.setArguments(bundle);
+        return dialogFragment;
+    }
+
+    public DialogFragment() {
+        // Required empty public constructor
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        builder.setTitle(title);
+        builder.setIcon(icon);
+        builder.setMessage(message);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Toast.makeText(getActivity(),"yes clicked",Toast.LENGTH_LONG).show();
+                positiveClickListener.OnPositiveButtonClicked();
+
+            }
+        });
+
+        builder.setNegativeButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+              //  Toast.makeText(getActivity(),"no clicked",Toast.LENGTH_LONG).show();
+                negativeClickListener.OnNegativeButtonClicked();
+            }
+        });
+
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //dismiss();
+                neutralClickListener.OnNeutralButtonClicked();
+            }
+        });
+        return builder.create();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dialog, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args=getArguments();
+        if(args!=null){
+            title=args.getString(ARG_TITLE);
+            message=args.getString(ARG_MESSAGE);
+            icon=args.getInt(ARG_ICON);
+        }
+    }
+
+    public interface OnPositiveClickListener{
+        void OnPositiveButtonClicked();
+    }
+
+    public interface OnNegativeClickListener{
+        void OnNegativeButtonClicked();
+    }
+    public interface OnNeutralClickListener{
+        void OnNeutralButtonClicked();
+    }
+}
---
+++ app/src/main/java/com/example/dialogfragments/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.dialogfragments;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements
+        DialogFragment.OnPositiveClickListener,
+        DialogFragment.OnNegativeClickListener,
+        DialogFragment.OnNeutralClickListener {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                AlertDialog dialog=new AlertDialog.Builder(MainActivity.this).
+//                        setTitle("Confirmation").setIcon(R.drawable.ic_launcher_background).setMessage("xxxxxxxxxxxxxxx").create();
+//                        dialog.show();
+                DialogFragment fragment=DialogFragment.newInstance("Confirmation",
+                        "Are you sure",
+                        R.drawable.ic_assignment_turned_in_black_24dp);
+                fragment.show(getSupportFragmentManager(),null);
+            }
+        });
+    }
+
+    @Override
+    public void OnPositiveButtonClicked() {
+        Toast.makeText(this,"yes clicked",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void OnNegativeButtonClicked() {
+        Toast.makeText(this,"no clicked",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void OnNeutralButtonClicked() {
+
+    }
+}
